3afbfcac1da41613d023e36b662139a34f182814
Boris Waaub <boris.waaub@gmail.com>
2022-05-12 11:59:10 +0200

    remove cors for dev env

--- vite.config.ts
+++ vite.config.ts
@@ -68,8 +68,8 @@ export default defineConfig({
   server: {
     host: true,
     port: 8080,
-    cors: true,
-    https: true,
+    cors: false,
+    https: false,
   },
   define: { "process.env": {} },
   resolve: {
