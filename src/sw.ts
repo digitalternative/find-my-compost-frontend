1c1f881cd965ef380a23376a253400a8e445a9b3
Boris Waaub <boris.waaub@gmail.com>
2022-05-19 14:50:56 +0200

    change test sw

--- src/sw.ts
+++ src/sw.ts
@@ -1,9 +1,6 @@
 /// <reference lib="webWorker" />
-import { skipWaiting, clientsClaim } from "workbox-core";
 import { precacheAndRoute, cleanupOutdatedCaches } from "workbox-precaching";
 
 declare const self: ServiceWorkerGlobalScope;
 cleanupOutdatedCaches();
 precacheAndRoute(self.__WB_MANIFEST);
-skipWaiting();
-clientsClaim();
